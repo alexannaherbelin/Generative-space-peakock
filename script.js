f4d2f9685b1b01dc45b285256009f70be8f24fa2
alexannaherbelin <alexannaherbelin@students.berkeley.net>
2026-04-09 18:41:49 +0000

    furthered

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function removePunctuation(str) {
     return cleanStr;
 }
 let nakedText = removePunctuation(lowerText);
-//console.log(nakedText);
 
 //count subsequent words
 let words = nakedText.split(/\s+/);
@@ -18,27 +17,65 @@ class WordInfo {
     constructor(word, appearances){
         this.word = word;
         this.appearances = appearances;
+        this.nextWords = {};
     }
     displayInfo(){
         console.log("The word '" + this.word + "' appears " + this.appearances + " times in the text.");
     }
+    addNextWord(nextWord){
+        if(this.nextWords[nextWord]){
+            this.nextWords[nextWord].frequency += 1;
+        } else {
+            this.nextWords[nextWord] = { word: nextWord, frequency: 1 };
+        }
+    }
 }
-words.forEach(word => {
+for(var i = 0; i < words.length - 1; i++){
+    let word = words[i];
+    let nextWord = words[i + 1];
+
+
     let same = false;
-    for(var i = 0; i < wordObjectArr.length; i++){
-        if(word === wordObjectArr[i].word){
+    for(var j = 0; j < wordObjectArr.length; j++){
+        if(word === wordObjectArr[j].word){
             same = true;
-            console.log(wordObjectArr[i].word)
-            wordObjectArr[i].appearances += 1
+            wordObjectArr[j].appearances += 1
+            wordObjectArr[j].addNextWord(nextWord);
             break;
         }
     }
     if(!same){
-        wordObjectArr.push(new WordInfo(word, 1));
+        let newWord = new WordInfo(word, 1);
+        newWord.addNextWord(nextWord);
+        wordObjectArr.push(newWord);
     }
-});
+}
 console.log(wordObjectArr);
 
+let numWordsGenerated = 10;
+let firstWord = wordObjectArr[Math.floor(Math.random() * wordObjectArr.length)];
+let currentWord = firstWord;
+for(var i = 0; i < numWordsGenerated; i++){
+    //console.log(currentWord.word);
+    
+    //frequency of next words array randomly generated
+    let nextWordsArr = Object.values(currentWord.nextWords);
+    if(nextWordsArr.length === 0){
+        break;
+    }
+
+   console.log(nextWordsArr);
+    let weightedNextWords = [];
+    nextWordsArr.forEach(nextWordObj => {
+        for(var j = 0; j < nextWordObj.frequency; j++){
+            weightedNextWords.push(nextWordObj.word);
+        }
+    });
+    console.log(weightedNextWords);
+    currentWord = wordObjectArr.find(wordObj => wordObj.word === weightedNextWords[Math.floor(Math.random() * weightedNextWords.length)]);
+
+    //currentWord = nextWordsArr[Math.floor(Math.random() * nextWordsArr.length)];
+}
 /*
 let freq = {}
 
